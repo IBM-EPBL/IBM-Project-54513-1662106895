b0ff5d2fd1ed40d18bf858471a5896c2f2eeb623
M-MUHAMMATHUABDULKALAM <113825227+M-MUHAMMATHUABDULKALAM@users.noreply.github.com>
2022-11-18 11:25:39 +0530

    Add files via upload

---
+++ Project development   phase/sprint 1/sprint 1.py
@@ -0,0 +1,46 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": []
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "code",
+      "execution_count": 1,
+      "metadata": {
+        "id": "mOaGECRIFYQu"
+      },
+      "outputs": [],
+      "source": [
+        "import numpy as np\n",
+        "import pandas as pd\n",
+        "import matplotlib.pyplot as plt\n",
+        "import seaborn as sns\n",
+        "import warnings\n",
+        "warnings.filterwarnings(\"ignore\")\n",
+        "import os"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "df= pd.read_csv(r'/content/water_dataX.csv')"
+      ],
+      "metadata": {
+        "id": "WyVdjbc4Fmu-"
+      },
+      "execution_count": 2,
+      "outputs": []
+    }
+  ]
+}
